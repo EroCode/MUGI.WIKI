0849848478f4d0c56c37389f65dc0875abe95fb1
Dimpurr <dimpurr@live.com>
2017-11-14 09:32:41 +0800

    postcss

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,17 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    { loader: 'css-loader', options: { importLoaders: 1 } },
+                    'postcss-loader'
+                  ]          
+            }
+        ]
     }
 };
