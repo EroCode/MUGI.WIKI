50a1820970baea7f62d4c597774df00cf02fd8c6
Dimpurr <dimpurr@live.com>
2017-11-17 11:13:57 +0800

    vue-router

--- src/hello.vue
+++ src/hello.vue
@@ -1,18 +1,29 @@
 <template>
 <div>
+    <h3>Components</h3>
     <p>{{ vue_hello }} MUGI</p>
-    <component></component>
+    <test></test>
+
+    <h3>Router</h3>
+    <p>
+        <router-link to="/test">Go to Test</router-link>
+    </p>
+    <router-view></router-view>
+
 </div>
 </template>
 
 <script>
-import component from './component.vue'
+import test from './test.vue'
 
 module.exports = {
     data: function() {
         return {
             vue_hello: "hello"
         }
+    },
+    components: {
+        test: test
     }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+
 import App from './hello.vue';
+import test from './test.vue';
+
+// vue-router
+
+Vue.use(VueRouter);
+
+const routes = [{
+    path: '/test',
+    component: test
+}];
+
+const router = new VueRouter({
+    routes // （缩写）相当于 routes: routes
+});
+
+// vue
 
 new Vue({
     el: '#app',
-    render: h => h(App)
+    render: h => h(App),
+    router
 });
